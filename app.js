8c4f7935ce3d41bccfca12f4ce6878b8d10015fb
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 15:26:34 -0500

    Basic request for first step in spotify auth

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const request = require('request');
 let app = express();
 
 app.get('/', (req,res) => {
-  
+  let spotifyClientID = process.env['SPOTIFY_CLIENT'];
+  let url = 'https://accounts.spotify.com/authorize?client_id=' + spotifyClientID + '&response_type=token&redirect_uri=http://localhost/login&show_dialog=true'
+
+  request( url, (error, response, body) => {
+    console.log('err:', error);
+    console.log('statusCode:', response.statusCode);
+    // console.log('response:', response);
+    // console.log('body:', body);
+  });
   res.send('Spotify Authentication Flow');
 })
 
