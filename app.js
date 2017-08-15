71d11a9840b84b0808c533770d3d13e7f8fdab97
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 19:41:47 -0500

    Replaced request call with a redirect to the spotify api
    This works, but I can't seem to get access to the URL with the token
    in it after the redirect back to my site.

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const request = require('request');
+const url = require('url');
 
 let app = express();
 
 app.get('/', (req,res) => {
   let spotifyClientID = process.env['SPOTIFY_CLIENT'];
-  let url = 'https://accounts.spotify.com/authorize?client_id=' + spotifyClientID + '&response_type=token&redirect_uri=http://localhost/login&show_dialog=true'
+  let url = 'https://accounts.spotify.com/authorize?client_id=' + spotifyClientID + '&response_type=token&redirect_uri=http://localhost:3000/login&show_dialog=true'
 
-  request( url, (error, response, body) => {
-    console.log('err:', error);
-    console.log('statusCode:', response.statusCode);
-    // console.log('response:', response);
-    // console.log('body:', body);
-  });
-  res.send('Spotify Authentication Flow');
+  res.redirect(301, url);
+});
+
+app.get('/login', (req,res) => {
+  // we should be able to grab the auth token from the url
+  let url_parts = url.parse(req.url, true);
+  console.log(url_parts);
+
+  res.sendStatus(200);
 })
 
 app.listen(3000, () => {
