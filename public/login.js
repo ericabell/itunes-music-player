42992a074a8516060c6831b156d15b2f0d034c47
Eric Abell <ericabell@users.noreply.github.com>
2017-08-14 20:08:51 -0500

    Auth works and basic request with token works!
    At this point, I think I can make whatever requests
    for data I need using the token.

--- public/login.js
+++ public/login.js
@@ -2,3 +2,20 @@
 // which contains the auth token
 
 console.log(window.location.hash);
+
+let accessToken = window.location.hash.substr(1).split('&')[0].split('=')[1];
+
+// assemble a request using fetch
+
+let getUserBasicURL = 'https://api.spotify.com/v1/me/';
+
+fetch(getUserBasicURL, {
+  method: 'get',
+  headers: {
+    'Authorization': 'Bearer ' + accessToken
+  }
+}).then( (response) => {
+  return( response.json() );
+}).then( (data) => {
+  console.log(data);
+});
